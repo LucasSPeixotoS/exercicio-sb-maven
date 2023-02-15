549357709e2f52162d62d7ba0039ecf2193fcd46
LucasSPeixotoS <lucasxvs5@gmail.com>
2023-02-15 16:09:21 -0300

    Feature Adicionando validação

--- src/main/java/com/springmaven/exerciciosbmaven/controller/ProductController.java
+++ src/main/java/com/springmaven/exerciciosbmaven/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.springmaven.exerciciosbmaven.controller;
 
 import com.springmaven.exerciciosbmaven.model.entity.Product;
 import com.springmaven.exerciciosbmaven.model.repository.ProductRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +14,7 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @PostMapping
-    public @ResponseBody Product newProduct(Product product){
+    public @ResponseBody Product newProduct(@Valid Product product){
         productRepository.save(product);
         return product;
     }
--- src/main/java/com/springmaven/exerciciosbmaven/model/entity/Product.java
+++ src/main/java/com/springmaven/exerciciosbmaven/model/entity/Product.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Product {
@@ -11,10 +15,16 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotNull
+    @NotBlank
     private String name;
 
+    @NotNull
+    @Min(0)
     private double price;
 
+    @Min(0)
+    @Max(1)
     private double discountPercentage;
 
     public Product() {
